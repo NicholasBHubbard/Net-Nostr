dc20bea2e5e8bb6703ae65e7f5117240d9f64868
Nicholas Hubbard <nicholashubbard@posteo.net>
2023-09-13 19:05:17 -0400

    arbitrary style change

--- lib/Net/Nostr/Event.pm
+++ lib/Net/Nostr/Event.pm
@@ -18,13 +18,13 @@ sub content    { shift->{content}    }
 sub sig        { shift->{sig}        }
 
 # setters
-sub set_id         { my ($self, $v) = @_; $self->{id} = $v         }
-sub set_pubkey     { my ($self, $v) = @_; $self->{pubkey} = $v     }
+sub set_id         { my ($self, $v) = @_; $self->{id}         = $v }
+sub set_pubkey     { my ($self, $v) = @_; $self->{pubkey}     = $v }
 sub set_created_at { my ($self, $v) = @_; $self->{created_at} = $v }
-sub set_kind       { my ($self, $v) = @_; $self->{kind} = $v       }
-sub set_tags       { my ($self, $v) = @_; $self->{tags} = $v       }
-sub set_content    { my ($self, $v) = @_; $self->{content} = $v    }
-sub set_sig        { my ($self, $v) = @_; $self->{sig} = $v        }
+sub set_kind       { my ($self, $v) = @_; $self->{kind}       = $v }
+sub set_tags       { my ($self, $v) = @_; $self->{tags}       = $v }
+sub set_content    { my ($self, $v) = @_; $self->{content}    = $v }
+sub set_sig        { my ($self, $v) = @_; $self->{sig}        = $v }
 
 sub new {
     my $class = shift;
